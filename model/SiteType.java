68a01f9f54b03d621bbf43b4c29aef482d4e6217
H-unt3R <44362059+H-unt3R@users.noreply.github.com>
2018-12-03 01:17:27 +0600

    Update SiteType.java

--- model/SiteType.java
+++ model/SiteType.java
@@ -15,20 +15,3 @@ public class SiteType {
         this.typeName = typeName;
     }
 }
-package model;
-public class SiteType {
-    private String typeName;
-    
-    public SiteType(String typeName){
-        this.setTypeName(typeName);
-    }
-    public SiteType(){
-        
-    }
-    public String getTypeName(){
-        return typeName;
-    }
-    public void setTypeName(String typeName){
-        this.typeName = typeName;
-    }
-}
